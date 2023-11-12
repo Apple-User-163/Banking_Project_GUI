168a09bc5147e4b52353c793f50dd39edac5506a
Apple-User-163 <avinumathew@gmail.com>
2023-11-13 00:07:39 +0530

    implemented rd

--- src/main/java/com/apple163/banking_project_gui/RD.java
+++ src/main/java/com/apple163/banking_project_gui/RD.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -16,8 +18,12 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class RD
 {
+    Boolean tme_check = false;
+    Boolean prin_check = false;
     public RD()
     {
         Stage primaryStage = new Stage();
@@ -30,6 +36,11 @@ public class RD
         Text sub_title = new Text("RECURRING DEPOSIT");
         ImageView imageView = new ImageView(logo);
         Button back_btn = new Button("BACK");
+        Text error = new Text();
+        TextField principal = new TextField();
+        TextField time = new TextField();
+        ComboBox<String> comboBox = new ComboBox<>();
+        Button calculate = new Button("CALCULATE");
 
         title.setFill(Color.rgb(82, 183, 136));
         title.setFont(Font.font("Unispace", 25));
@@ -39,6 +50,10 @@ public class RD
         sub_title.setFont(Font.font("Unispace", 40));
         sub_title.setX(60);
         sub_title.setY(120);
+        error.setFont(Font.font("Unispace", 20));
+        error.setY(610);
+        error.setX(500);
+        error.setFill(Color.RED);
 
         imageView.setX(10);
         imageView.setY(5);
@@ -80,12 +95,140 @@ public class RD
         primaryStage.setFullScreenExitHint("");
         primaryStage.setResizable(false);
 
+        principal.setLayoutX(500);
+        principal.setLayoutY(400);
+        principal.setPrefSize(500, 60);
+        principal.setPromptText("Monthly Deposit");
+        principal.setFont(Font.font("Unispace", 20));
+        principal.setStyle("-fx-background-color: transparent; -fx-border-color: rgb(82, 183, 136); -fx-border-width: 2px; -fx-border-radius: 5px; -fx-background-radius: 5px; -fx-text-fill: rgb(82, 183, 136);");
+        time.setLayoutX(500);
+        time.setLayoutY(500);
+        time.setPrefSize(500, 60);
+        time.setPromptText("Time Period");
+        time.setFont(Font.font("Unispace", 20));
+        time.setStyle("-fx-background-color: transparent; -fx-border-color: rgb(82, 183, 136); -fx-border-width: 2px; -fx-border-radius: 5px; -fx-background-radius: 5px; -fx-text-fill: rgb(82, 183, 136);");
+
+        calculate.setLayoutX(500);
+        calculate.setLayoutY(650);
+        calculate.setPrefSize(500, 60);
+        calculate.setFont(Font.font("Unispace", 20));
+        calculate.setTextFill(Color.rgb(82, 183, 136));
+        calculate.setStyle("-fx-background-color: transparent; -fx-border-color: rgb(82, 183, 136); -fx-border-width: 2px; -fx-border-radius: 5px; -fx-background-radius: 5px;");
+        calculate.setOnMouseEntered((MouseEvent event) -> {
+            calculate.setTextFill(Color.rgb(1, 33, 24));
+            calculate.setStyle("-fx-background-color: rgb(45, 106, 79); -fx-border-color: rgb(45, 106, 79); -fx-border-width: 2px; -fx-border-radius: 5px; -fx-background-radius: 5px;");
+        });
+        calculate.setOnMouseExited((MouseEvent event) -> {
+            calculate.setTextFill(Color.rgb(82, 183, 136));
+            calculate.setStyle("-fx-background-color: transparent; -fx-border-color: rgb(82, 183, 136); -fx-border-width: 2px; -fx-border-radius: 5px; -fx-background-radius: 5px;");
+        });
+        calculate.setOnAction((ActionEvent event) -> {
+            error.setText("");
+            int upper, lower;
+
+            if (time.getText().isEmpty() || principal.getText().isEmpty() || comboBox.getValue() == null) {
+                error.setText("PLEASE FILL ALL THE FIELDS");
+            }
+            else{
+                String type = comboBox.getValue();
+                switch (type) {
+                    case "₹70,001 to ₹1,00,000; at 6%; for 1 to 24 months": upper = 24; break;
+                    case "₹40,001 to ₹70,000; at 8%; for 1 to 30 months" : upper = 30; break;
+                    case "₹10,001 to ₹40,000; at 10%; for 1 to 54 months" : upper = 54; break;
+                    case "₹100 to ₹10,000; at 12%; for 1 to 120 months" : upper = 120; break;
+                    default : upper = 0;
+                }
+                if (Integer.parseInt(time.getText()) > upper || Integer.parseInt(time.getText()) < 1) {
+                    error.setText("PLEASE ENTER A VALID TIME PERIOD");
+                    tme_check = false;
+                }
+                else {
+                    tme_check = true;
+                }
+                switch (type) {
+                    case "₹70,001 to ₹1,00,000; at 6%; for 1 to 24 months" : {
+                        upper = 100000;
+                        lower = 70001;
+                        break;
+                    }
+                    case "₹40,001 to ₹70,000; at 8%; for 1 to 30 months" : {
+                        upper = 70000;
+                        lower = 40001;
+                        break;
+                    }
+                    case "₹10,001 to ₹40,000; at 10%; for 1 to 54 months" : {
+                        upper = 40000;
+                        lower = 10001;
+                        break;
+                    }
+                    case "₹100 to ₹10,000; at 12%; for 1 to 120 months" : {
+                        upper = 10000;
+                        lower = 100;
+                        break;
+                    }
+                    default : lower = 0;
+                }
+                if (Integer.parseInt(principal.getText()) > upper || Integer.parseInt(principal.getText()) < lower) {
+                    error.setText("PLEASE ENTER A VALID PRINCIPAL AMOUNT");
+                    prin_check = false;
+                }
+                else {
+                    prin_check = true;
+                }
+
+                if (tme_check && prin_check) {
+                    error.setText("");
+                    int pri = Integer.parseInt(principal.getText());
+                    int tim = Integer.parseInt(time.getText());
+                    double amount = getAmount(type, pri, tim);
+                    double interest = amount - (pri * tim);
+                    root.getChildren().removeAll(principal, time, comboBox, calculate);
+                    Text amount_text = new Text("Amount: ₹" + amount);
+                    Text interest_text = new Text("Interest: ₹" + interest);
+                    amount_text.setFont(Font.font("Unispace", 30));
+                    amount_text.setFill(Color.rgb(82, 183, 136));
+                    amount_text.setX(500);
+                    amount_text.setY(400);
+                    interest_text.setFont(Font.font("Unispace", 30));
+                    interest_text.setFill(Color.rgb(82, 183, 136));
+                    interest_text.setX(500);
+                    interest_text.setY(500);
+                    root.getChildren().addAll(amount_text, interest_text);
+                }
+            }
+        });
+
+        comboBox.setEditable(false);
+        comboBox.setLayoutX(500);
+        comboBox.setLayoutY(300);
+        comboBox.setPrefSize(500, 60);
+        comboBox.setPromptText("Investment Options");
+        comboBox.getItems().addAll("₹70,001 to ₹1,00,000; at 6%; for 1 to 24 months", "₹40,001 to ₹70,000; at 8%; for 1 to 30 months", "₹10,001 to ₹40,000; at 10%; for 1 to 54 months", "₹100 to ₹10,000; at 12%; for 1 to 120 months");
+
+        root.getChildren().add(error);
+        root.getChildren().add(calculate);
         root.getChildren().add(sub_title);
         root.getChildren().add(back_btn);
         root.getChildren().add(stackPane);
         root.getChildren().add(title);
         root.getChildren().add(imageView);
+        root.getChildren().add(principal);
+        root.getChildren().add(time);
+        root.getChildren().add(comboBox);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/resources/com/apple163/banking_project_gui/dropdown.css")).toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    private static double getAmount(String type, int pri, int tim) {
+        int rate;
+        switch (type) {
+            case "₹70,001 to ₹1,00,000; at 6%; for 1 to 24 months": rate = 6; break;
+            case "₹40,001 to ₹70,000; at 8%; for 1 to 30 months" : rate = 8; break;
+            case "₹10,001 to ₹40,000; at 10%; for 1 to 54 months" : rate = 10; break;
+            case "₹100 to ₹10,000; at 12%; for 1 to 120 months" : rate = 12; break;
+            default : rate = 0;
+        }
+        double Interest = pri * ((tim *( tim + 1)) / 24.0) * (rate / 100.0);
+        return (pri * tim) + Interest;
     }
 }
